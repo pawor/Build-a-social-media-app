2ec32c9181bec6d7b040d87301b2a6f5d40ccacd
pawor <pawor.emi@gmail.com>
2021-10-23 22:59:37 +0700

    create auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,18 +48,72 @@ const authController = {
             return res.status(500).json({msg: err.message})
         }
     },
+    
     login: async (req,res) => {
-        try{}catch (err){
+        try{
+            const { email, password } = req.body
+
+            const user = await Users.findOne({email})
+            .populate("followers following", "-password")
+
+            if(!user) return res.status(400).json({msg: "This email does not exist."})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Password is incorrect."})
+
+            const access_token = createAccessToken({id: user._id})
+            const refresh_token = createRefreshToken({id: user._id})
+
+            res.cookie('refreshtoken', refresh_token, {
+                httpOnly: true,
+                path: '/api/refresh_token',
+                maxAge: 30*24*60*60*1000 //30days
+            })
+
+            res.json({
+                msg: 'Login Success!',
+                access_token,
+                user: {
+                    ...user._doc,
+                    password: ''
+                }
+            })
+        }catch (err){
             return res.status(500).json({msg: err.message})
         }
     },
+
     logout: async (req,res) => {
-        try{}catch (err){
+        try{
+            res.clearCookie('refreshtoken', {path: '/api/refresh_token'})
+            return res.json({msg: "Logged out!"})
+        }catch (err){
             return res.status(500).json({msg: err.message})
         }
     },
+
     generateAccessToken: async (req,res) => {
-        try{}catch (err){
+        try{
+            const rf_token = req.cookies.refreshtoken
+            if(!rf_token)  return res.status(400).json({msg: "Please login now."})
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async(err, result) => {
+                if(err) return res.status(400).json({msg: "Please login now."})
+
+                const user = await Users.findById(result.id).select("-password")
+                .populate('followers following', '-password')
+
+                if(!user) return res.status(400).json({msg: "This does not exist."})
+
+                const access_token = createAccessToken({id: result.id})
+
+                res.json({
+                    access_token,
+                    user
+                })
+            })
+
+        }catch (err){
             return res.status(500).json({msg: err.message})
         }
     }
